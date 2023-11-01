c9e14cf197accc4b4578bb454dc8b27d3d2efc91
Enes <enesvelovic@gmail.com>
2023-11-01 12:53:37 -0400

    Add Feedings to Pokemon Details View

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+  ('N', 'Nanab Berry'),
+  ('R', 'Razz Berry'),
+  ('P', 'Pinap Berry'),
+)
+
 class Pokemon(models.Model):
   name = models.CharField(max_length=100)
   breed = models.CharField(max_length=100)
@@ -12,3 +18,17 @@ class Pokemon(models.Model):
 
   def get_absolute_url(self):
     return reverse('pokemon-detail', kwargs={'pokemon_id': self.id})
+
+
+class Feeding(models.Model):
+  date = models.DateField('Date Caught')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+
+  pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
