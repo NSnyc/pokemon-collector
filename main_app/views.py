f29cfd14c94b3d4e20728cf7b197a104d81d3785
Enes <enesvelovic@gmail.com>
2023-11-01 01:17:16 -0400

    Add View for About and Upload Images

--- main_app/views.py
+++ main_app/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Hello Future Champions! ᓚᘏᗢ</h1>')
+
+def about(request):
+  return render(request, 'about.html')
