6201b230247262bb0dcdb1827c8be2f390dc0c91
Enes <enesvelovic@gmail.com>
2023-11-01 02:42:10 -0400

    Add Pokemon Details View

--- main_app/views.py
+++ main_app/views.py
@@ -2,20 +2,22 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-# Add the Pokemon class & list and view function below the imports
-class Pokemon:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
+from main_app.models import Pokemon
 
-pokemon = [
-  Pokemon('Lolo', 'tabby', 'Kinda rude.', 3),
-  Pokemon('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-  Pokemon('Fancy', 'bombay', 'Happy fluff ball.', 4),
-  Pokemon('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
+# Add the Pokemon class & list and view function below the imports
+# class Pokemon:  # Note that parens are optional if not inheriting from another class
+#   def __init__(self, name, breed, description, age):
+#     self.name = name
+#     self.breed = breed
+#     self.description = description
+#     self.age = age
+
+# pokemon = [
+#   Pokemon('Lolo', 'tabby', 'Kinda rude.', 3),
+#   Pokemon('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+#   Pokemon('Fancy', 'bombay', 'Happy fluff ball.', 4),
+#   Pokemon('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+# ]
 
 # Define the home view
 def home(request):
@@ -25,4 +27,9 @@ def about(request):
   return render(request, 'about.html')
 
 def pokemon_index(request):
+  pokemon=Pokemon.objects.all()
   return render(request, 'pokemon/index.html', {'pokemon': pokemon})
+
+def pokemon_detail(request, pokemon_id):
+  pokemon = Pokemon.objects.get(id=pokemon_id)
+  return render(request, 'pokemon/detail.html', {'pokemon': pokemon})
